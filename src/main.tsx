252a8928ca74ba6da1f93dfae41e359936b2da3d
[dyad] <ussloja@gmail.com>
2026-03-16 01:12:32 -0300

    [dyad] Ajuste na posição das notificações para o topo da tela - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <AuthProvider>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App />
-        <Toaster />
+        <Toaster position="top-center" />
       </ThemeProvider>
     </AuthProvider>
   </QueryClientProvider>
